492930e16dbb92be52e1492f2527ccf3414febc6
CaptnB <captnb@gmail.com>
2013-04-18 17:14:12 -0400

    Fixes #57

--- src/com/pathfindersdk/stats/Dice.java
+++ src/com/pathfindersdk/stats/Dice.java
@@ -2,34 +2,43 @@ package com.pathfindersdk.stats;
 
 import java.util.Random;
 
+import com.pathfindersdk.utils.ArgChecker;
+
 
 final public class Dice implements Rollable
 {
   final private int numbers;
   final private int faces;
+  final private Integer seed;
   
   public Dice(int numbers, int faces)
   {
-    if(numbers > 0)
-      this.numbers = numbers;
-    else
-      this.numbers = 1;
-    
-    if(faces > 0)
-      this.faces = faces;
-    else
-      this.faces = 1;
+    this(numbers, faces, null);
+  }
+  
+  public Dice(int numbers, int faces, Integer seed)
+  {
+    ArgChecker.checkIsPositive(numbers);
+    ArgChecker.checkIsPositive(faces);
+
+    this.numbers = numbers;
+    this.faces = faces;
+    this.seed = seed;
   }
   
   @Override
   public int roll()
   {
+    Random random;
+    if(seed != null)
+      random = new Random(seed);
+    else
+      random = new Random();
+
     int roll = 0;
-    Random random = new Random();
     for(int i = 0; i < numbers; i++)
-    {
       roll += random.nextInt(faces) + 1;
-    }
+
     return roll;
   }
 }
